b7de199a4ecced4ea99b25dc70ba9fba282f123b
Vitor H Oliveira <vitor_osantos@hotmail.com>
2022-03-08 08:42:04 -0300

    Alterando a pagina 404

--- src/Pages/pageNotFound.js
+++ src/Pages/pageNotFound.js
@@ -1,10 +1,14 @@
 import { Notfound } from "../components/0-notFound/notFound-style"
+import { ButtonSec } from "../components/4-gerais/botao/botao-secondary/ButtonSec"
+
+import { BsArrowRight } from 'react-icons/bs';
 
 function PageNotFound(){
   return(
     <Notfound>
       <h1>404 <span>.</span></h1>
-      <p>Houston, we have a problem</p>
+      <p>Não há lugar como nosso lar...</p>
+      <ButtonSec href="https://vitor0sant0s.vercel.app/">Voltar <BsArrowRight/> </ButtonSec>
     </Notfound>
   )
 }
---
+++ src/components/4-gerais/botao/botao-secondary/ButtonSec.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ButtonSec = styled.a`
+  
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  text-decoration: none;
+
+  padding: 0.5em;
+
+  border-bottom: 2px solid #0070F3;
+`
